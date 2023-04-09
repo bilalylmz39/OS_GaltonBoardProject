fa7df4403c7f575b7ae73207c7d9c9524b0cef86
Yusuf Bilal YILMAZ <yusufbilaly29@gmail.com>
2023-04-09 21:02:09 +0300

    Test file is edited

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,10 +3,12 @@ package org.example;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
+
+
 public class Test {
     public static void main(String[] args) {
-        Options options = new Options();
-        CmdLineParser parser = new CmdLineParser(options);
+        Options opt = new Options();
+        CmdLineParser parser = new CmdLineParser(opt);
 
         try {
             parser.parseArgument(args);
@@ -15,8 +17,8 @@ public class Test {
             parser.printUsage(System.err);
             System.exit(1);
         }
-        int numThread = options.numThread;
-        int numBins = options.numBins;
+        int numThread = opt.numThread;
+        int numBins = opt.numBins;
 
         GaltonBoard galtonBoard = new GaltonBoard(numBins, numThread);
 
